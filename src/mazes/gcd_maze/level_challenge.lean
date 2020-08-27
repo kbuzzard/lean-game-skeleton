71cc76602699d96e7a82a936b10234075de8af01
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-08-27 19:33:31 +0100

    solve challenge

--- src/mazes/gcd_maze/level_challenge.lean
+++ src/mazes/gcd_maze/level_challenge.lean
@@ -22,18 +22,64 @@ Solver remark : there are infinitely many mazes.
 /- Lemma : no-side-bar
 Can you prove you can escape in the general case?
 -/
-theorem challenge (a b : ℕ) : can_escape a b 0 :=
+theorem challenge (A B : ℕ) : can_escape A B 0 :=
 begin
-  have solution : can_escape a b (nat.gcd a b),
+  have solution : can_escape A B (nat.gcd A B),
     out,
-  sorry
-
-
-
-
-
-
-
-
+  have hs : ∀ t l : ℕ, can_escape A B t → can_escape A B (t + l*A),
+  { intros t l,
+    induction l with d hd,
+    { intro h, convert h, simp },
+    intro h,
+    specialize hd h,
+    s,
+    convert hd,
+    ring
+  },
+  have hns : ∀ t : ℤ, ∀ l : ℤ, can_escape A B t → can_escape A B (t + l * A),
+  { intros t l,
+    apply int.induction_on l; clear l,
+    { intro h, convert h, ring},
+    { intros d hd, 
+      intro h, specialize hd h,
+      s,
+      convert hd,
+      ring,
+    },
+    { intros d hd, 
+      intro h, specialize hd h,
+      n,
+      convert hd using 1,
+      ring,
+    } },
+  have hew : ∀ t : ℤ, ∀ m : ℤ, can_escape A B t → can_escape A B (t + m * B),
+  { intros t l,
+    apply int.induction_on l; clear l,
+    { intro h, convert h, ring},
+    { intros d hd, 
+      intro h, specialize hd h,
+      w,
+      convert hd,
+      ring,
+    },
+    { intros d hd, 
+      intro h, specialize hd h,
+      e,
+      convert hd using 1,
+      ring,
+    } },
+  suffices : ∃ L M : ℤ, L * A + M * B = nat.gcd A B,
+  { rcases this with ⟨L, M, h⟩,
+    rw ← h at solution,
+    specialize hew _ (-M) solution,
+    simp at hew,
+    specialize hns _ (-L) hew,
+    convert hns,
+    ring,
+  },
+  have h := nat.gcd_eq_gcd_ab A B,
+  rw h,
+  use [A.gcd_a B, A.gcd_b B],
+  ring,
 end
 
